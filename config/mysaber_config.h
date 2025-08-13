37a628328f2710478824f1d12c57ccab257d81e3
herotech-engineering <contact@herotech.engineering>
2025-08-13 16:29:13 +0800

    Update mysaber_config.h

--- config/mysaber_config.h
+++ config/mysaber_config.h
@@ -1,26 +1,24 @@
 #ifdef CONFIG_TOP
 #include "proffieboard_v3_config.h"
-#define NUM_BLADES 2
-#define NUM_BUTTONS 0
-#define VOLUME 2400
-const unsigned int maxLedsPerStrip = 144;
+#define NUM_BLADES 1
+#define NUM_BUTTONS 1
+#define VOLUME 2000
 #define CLASH_THRESHOLD_G 100.0
 #define ENABLE_AUDIO
 #define ENABLE_MOTION
-#define ENABLE_WS2811
 #define ENABLE_SD
-#define MOTION_TIMEOUT 60 * 100 * 1000  // 100 minutes before motion timeout
-#define IDLE_OFF_TIME 60 * 100 * 1000   // 100 minutes idle before powering down
+#define MOTION_TIMEOUT 60 * 10 * 1000  // 10 minutes before motion timeout
+#define IDLE_OFF_TIME 60 * 10 * 1000   // 10 minutes idle before powering down
 #endif
 
 #ifdef CONFIG_PROP
-#include "../props/spinning_lightsaber.h"  // Include our custom prop
+#include "../props/project_jedi.h"  // Include our custom prop
 #endif
 
 #ifdef CONFIG_PRESETS
 Preset presets[] = {
   {"Skywalker", "tracks/venus.wav", 
-  StyleNormalPtr<CYAN, WHITE, 300, 800>(), StyleNormalPtr<CYAN, WHITE, 300, 800>(), "Ignition" }
+  StyleNormalPtr<CYAN, WHITE, 300, 800>(), "Ignition" }
 };
 
 struct myLED {
@@ -35,7 +33,7 @@ struct myLED {
 };
 
 BladeConfig blades[] = {
- { 0, SimpleBladePtr<myLED, NoLED, NoLED, NoLED, bladePowerPin5, -1, -1, -1>(), 
+ { 0, 
 SimpleBladePtr<myLED, NoLED, NoLED, NoLED, bladePowerPin6, -1, -1, -1>(),
  CONFIGARRAY(presets)}
 };
